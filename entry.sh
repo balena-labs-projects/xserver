5f38db0fcec32b0c0a7528ec1bfcf27ef897c1c0
Flynn Joffray <nucleardreamer@gmail.com>
2021-07-06 13:57:21 -0700

    Adding balena entry.sh for udev mounting

--- entry.sh
+++ entry.sh
@@ -1,5 +1,19 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
+/usr/bin/entry.sh echo "Running balena base image entrypoint..."
+
+export DBUS_SYSTEM_BUS_ADDRESS=unix:path=/host/run/dbus/system_bus_socket
 
 echo "balenaBlocks x11 version: $(cat VERSION)"
 
-exec startx -- -listen tcp
+# If the vcgencmd is supported (i.e. RPi device) - check enough GPU memory is allocated
+if command -v vcgencmd &> /dev/null
+then
+	echo "Checking GPU memory"
+    if [ "$(vcgencmd get_mem gpu | grep -o '[0-9]\+')" -lt 128 ]
+	then
+	echo -e "\033[91mWARNING: GPU MEMORY TOO LOW"
+	fi
+fi
+
+startx -- -listen tcp
